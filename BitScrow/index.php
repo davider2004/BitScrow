622fcca8d132283e670c82e86f86c30686ea4936
Davide Ramondetti <github@ramondettidavide.com>
2017-10-26 21:43:13 +0200

    New version BETA 1 released

---
+++ BitScrow/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<!--
+	YOU WANT TO COPY THIS PAGE THEME OR SEE HOW DOES THE ESCROW WORKS?
+	NO PROBLEM! See this on Github:
+	
+	http://bitscrow.one/github
+-->
+<html>
+	<head>
+		<title>BitScrow - Bitcoin escrow service</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-loading">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Say bye to scammers! <br />This is<br />
+						BITSCROW</h1>
+						<p>Are you continue getting scammed? Do you want to be sure that the transaction or the product are you going to buy is safe?<br />
+						If yes, STOP LOSING MONEY OR TIME, USE THIS NEW ESCROW SERVICE. <a href="what-is-bitscrow.php">More informations</a>.</p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+				<?php include 'assets/elements/header.php'; ?>
+				
+				<!-- Nav -->
+				<?php include 'assets/elements/nav.php'; ?>
+				
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									<span class="date">BITSCROW</span>
+									<h2><a href="#">WELCOME TO<br />
+									BITSCROW!</a></h2>
+									<p>Bitscrow is a very high-speed and safe escrow service.<br />
+									Then, what are you waiting for? Read the instructions and RUN A TRANSACTION!</p>
+								</header>
+								<a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a>
+								<ul class="actions">
+									<li><a href="what-is-bitscrow.php" class="button big">WHAT IS BITSCROW?</a></li>
+								</ul>
+							</article>
+
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										<span class="date">SAFETY</span>
+										<h2><a href="#">#1 SAFEST ESCROW<br />
+										HIGH CONTROLS</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/lock.jpg" alt="" /></a>
+									<p>Our escrow is 100% safe for buyer and seller. Once the buyer say that the service doesn't work we will manually contact the seller and test ourself the product.</p>
+									<p>We also provide SSL-safe website protection and a 1Tbps DDoS Protection guaranteed and also a 24/7 support.</p>
+									<ul class="actions">
+										<li><a href="what-is-bitscrow.php" class="button">Learn more about BitScrow</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date">FEES</span>
+										<h2><a href="#">Very low<br />
+										transaction fees</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/low-fees.jpg" alt="" /></a>
+									<p>As you known nothing in the world is free, escrow also need to paid for the escrow service, but we made <b>VERY LOW TRANSACTION FEES</b>!</p>
+									<p>Our fees are only <b>0.0004 BTC</b> (exclude mining fees) and they decrase of <b>-0.5% EVERY TRANSACTION DONE SUCCESSFULLY</b>!</p>
+									<ul class="actions">
+										<li><a href="what-is-bitscrow.php" class="button">Learn more about BitScrow</a></li>
+									</ul>
+								</article>
+							</section>
+
+						<!-- Footer -->
+							<footer>
+								<div class="pagination">
+									<!--<a href="#" class="previous">Prev</a>-->
+									<a href="#" class="page active">1</a>
+								</div>
+							</footer>
+
+					</div>
+
+				<!-- Footer -->
+					<?php include 'assets/elements/footer.php'; ?>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ BitScrow/what-is-bitscrow.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<!--
+	YOU WANT TO COPY THIS PAGE THEME OR SEE HOW DOES THE ESCROW WORKS?
+	NO PROBLEM! See this on Github:
+	
+	http://bitscrow.one/github
+-->
+<html>
+	<head>
+		<title>What is Bitscrow? | BitScrow Escrow Services</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-loading">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+				<?php include 'assets/elements/header.php'; ?>
+
+				<!-- Nav -->
+				<?php include 'assets/elements/nav.php'; ?>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Post -->
+							<section class="post">
+								<header class="major">
+									<span class="date">WHAT IS BITSCROW?</span>
+									<h1>WHAT IS<br />
+									BITSCROW</h1>
+									<p>Hi user, I think you be scammed a lot of times or you want your Bitcoin Transactions beeing safe.<br />
+									That's what the escrow do. An <b>escrow service</b> works like this: first the user deposit to the escrow, <br />
+									then the seller ship/give the item and then you select if release or not the money to the seller.<br />
+									<b>BUT THAT IS THE PROBLEM!</b>. A LOT OF SELLERS REFUSE USING AN ESCROW BECAUSE THE USER CAN SCAM THE SELLER, <br />
+									So we developed a new fantastic way to stop this: the user and the seller have timeouts of 48 hours to release (in case of the buyer) <br />
+									or give the product (in the case of the seller), in the case of the seller, the transaction will be blocked and the money gived back to the buyer,<br />
+									or if the buyer exceed the timeout, the money will be automatically released to the seller. Then, if the buyer give a NO RELEASE question, <br />
+									instead of other Escrows we MANUALLY CHECK if the product is working or not. THIS IS A WAY TO BE SAFE DURING TRANSACTIONS. But now here are the informations:</p>
+								</header>
+								<div class="image main"><img src="images/pic01.jpg" alt="" /></div>
+								<p><b>1. LOW FEES</b></p>
+								<p>No real escrow services are free, all require some fees, but sometimes they are <b>TOO HIGH</b>, like some escrow requires the 50% OF THE PRICE OF THE TRANSACTION.</p>
+								<p>But we are DIFFERENT, our fees are only of <b>0.0004 BTC / transaction</b> (+ additional 0.00001 BTC / product check; + mining fees of 0.00002 BTC). THIS IS A VERY LOW PRICE.</p>
+								<p>You can also choose <b>WHO PAY THE FEES OR DIVIDE THIS FOR THE TWO PEOPLE</b>. Then for user that have done more thank <b>10 SUCCESSFULL TRANSACTIONS</b> they have to pay <b>ONLY -0.5% / transaction and NO MINING FEES</b>!</p>
+								<p></p>
+								<p><b>2. THE SAFETY</b></p>
+								<p>Sometimes escrows have difficult to check products because they haven't access to seller-buyer communications. <b>SO WE CREATE A FREE LIVE CHAT IN OUR WEBSITE FOR COMMUNICATION BUYER-SELLER AND THE SELLER NEEDS TO UPLOAD THE SHIPPING TRACKING OR THE FILES ON OUR WEBSITE! SO YOU ARE SAFE!</b></p>
+								<p></p>
+								<p><b>3. NO HUMAN INTERVENTION REQUIRED FOR NORMAL TRANSACTIONS</b></p>
+								<p>If the transaction doesn't have problems (like buyer says: "product not working", etc.) <b>NO HUMAN INTERVENTION REQUIRED</b>, so the transaction became faster!</p>
+								<p></p>
+								<p><b>4. NO PAYMENT LIMITS</b></p>
+								<p>Some escrow makes limits like minimum transaction is 0.001 BTC and maximum is 1 BTC, but <b>WE DO NOT HAVE LIMITS</b>, so you can pay ANY amount of money by staying safe.</p>
+								<p></p>
+								<p><b>5. NO-SCAM ESCROW / WE ARE OPENSOURCE!</b></p>
+								<p>On the web there are a lot of escrow, but some of them only steal your money. WE DON'T BECAUSE OUR SOURCE CODE IS OPEN-SOURCE. So you can see our <b>SOURCE CODE ON GITHUB</b> by using the link you find the footer or on the menu!</p>
+								<p>We also use a verified CRYPTOPAY.ME wallet with high-speed apis and servers.</p>
+								<p></p>
+								<p><b>SO WHAT ARE YOU WAITING FOR?</b></p>
+								<ul class="actions">
+									<li><a href="start.php" class="button">START A TRANSACTION!</a></li>
+								</ul>
+							</section>
+
+					</div>
+
+				<!-- Footer -->
+				<?php include 'assets/elements/footer.php'; ?>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
